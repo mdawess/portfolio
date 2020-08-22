a468035e9cf4662a57b2306d80014580e12a1463
mdawess <64979871+mdawess@users.noreply.github.com>
2020-08-22 12:29:40 -0400

    added ability to make a dict of tickers & prices

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from stonk import stk
+from stonk import Portfolio
 
 BASE = 1
 
 portfolio = ["CGC", "TD.TO", "WCN", "BMO.TO", "BNS.TO", "NFI.TO", "VCN.TO", 
             "VOO", "SPY", "PM"]
 
-test = stk(portfolio)
+current_shares = [10, 20, 35, 5, 12, 1, 11, 6, 1, 1]
+
+test = Portfolio(portfolio)
 print(test.current_prices())
--- stonk.py
+++ stonk.py
@@ -1,17 +1,23 @@
 import yfinance as yf
-import forex-python
+from forex_python.converter import CurrencyRates
 
-class stk:
+class Portfolio:
 
     def __init__(self, stocks):
         
         self.stocks = stocks
 
-    def convert_cad(self) -> dict:
+    def convert_cad(self, ttp) -> dict:
         """Returns a new dictionary with all prices in CAD."""
+        c = CurrencyRates()
+        CAD = c.get_rates("USD")["CAD"]
 
+        for stock in ttp:
+            if stock[-2:] != "TO":
+                ttp[stock] = ttp[stock] * CAD
 
-
+        return ttp
+        
     def current_prices(self) -> dict:
         """Returns the current prices attached to the individual ticker in a dictionary."""
 
@@ -22,8 +28,13 @@ class stk:
             price = initialize.history(period="max")["Close"][-1]
             ticker_to_price[stock] = price
 
+        ticker_to_price = self.convert_cad(ticker_to_price)
+
         return ticker_to_price
 
-test = stk(["MSFT", "AAPL", "CGC"])
+    def portfolio_value(self) -> float:
+        """Returns the total value of all current holdings"""
 
-print(test.current_prices())
+    
+
+
