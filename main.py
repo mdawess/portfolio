2ed07676ae628b6278f890738300ee5de92ad1de
mdawess <64979871+mdawess@users.noreply.github.com>
2020-08-27 16:30:06 -0400

    added getter & setter

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ import tkinter as tk
 BASE = 1
 
 portfolio = ["CGC", "TD.TO", "WCN", "BMO.TO", "BNS.TO", "NFI.TO", "VCN.TO", 
-            "VOO", "SPY", "PM"]
+            "VOO", "SPY", "PM", "TSLA"]
 
 current_shares = [10, 20, 35, 5, 12, 1, 11, 6, 1, 1]
 
-test = Portfolio(portfolio)
-print(test.current_prices())
+test = Portfolio()
+test.add_ticker("MSFT")
+test.add_ticker("AAPL")
+test.add_ticker("TSLA")
+test.check_portfolio()
+
+
+test.add_ticker(portfolio)
+test.check_portfolio()
+
+test.remove_ticker(["MSFT", "CGC", "AMZN"])
+test.check_portfolio()
--- stonk.py
+++ stonk.py
@@ -2,10 +2,49 @@ import yfinance as yf
 from forex_python.converter import CurrencyRates
 
 class Portfolio:
-
-    def __init__(self, stocks):
+    
+    def __init__(self):
         
-        self.stocks = stocks
+        self.stocks = []
+
+    def add_ticker(self, ticker) -> None:
+        """Adds a ticker or tickers to the list of stocks. Can either be a single ticker
+        or a list of tickers.
+        """ 
+        # Add ability to add X number of shares as well
+        if type(ticker) == str:
+            if ticker not in self.stocks:
+                self.stocks.append(ticker)
+            else:
+                print(ticker + " has already been added to the portfolio")
+        if type(ticker) == list:
+            for t in ticker:
+                if t not in self.stocks:
+                    self.stocks.append(t)
+                else:
+                    print(t + " has already been added to the portfolio")
+
+    def remove_ticker(self, ticker) -> None:
+        """Removes a ticker or tickers from the list from the list. Can be either a
+        single ticker or a list.
+        """
+        # Add ability to remove X number of shares as well
+        if type(ticker) == str:
+            if ticker in self.stocks:
+                self.stocks.remove(ticker)
+            else:
+                print(ticker + " is not in the portfolio")
+        if type(ticker) == list:
+            for t in ticker:
+                if t in self.stocks:
+                    self.stocks.remove(t)
+                else:
+                    print(t + " is not in the portfolio")
+
+    def check_portfolio(self) -> list:
+        """Returns the current holdings and their most recent price."""
+        
+        print(self.stocks)
 
     def convert_cad(self, ttp) -> dict:
         """Returns a new dictionary with all prices in CAD."""
