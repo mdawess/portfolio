33db0856ce762b0f8618021c97d09f43cc5bf695
mdawess <64979871+mdawess@users.noreply.github.com>
2020-08-27 21:13:00 -0400

    enabled ability to look at current holdings number of shares and prices

--- main.py
+++ main.py
@@ -3,20 +3,26 @@ import tkinter as tk
 
 BASE = 1
 
-portfolio = ["CGC", "TD.TO", "WCN", "BMO.TO", "BNS.TO", "NFI.TO", "VCN.TO", 
+PORTFOLIO = ["CGC", "TD.TO", "WCN", "BMO.TO", "BNS.TO", "NFI.TO", "VCN.TO", 
             "VOO", "SPY", "PM", "TSLA"]
 
 current_shares = [10, 20, 35, 5, 12, 1, 11, 6, 1, 1]
 
 test = Portfolio()
 test.add_ticker("MSFT")
-test.add_ticker("AAPL")
 test.add_ticker("TSLA")
+
+
+test.check_portfolio()
+
+# test.remove_ticker("MSFT", 3)
+test.remove_ticker("AAPL", 3)
+test.check_portfolio()
+
+# test.remove_ticker("MSFT", 9)
 test.check_portfolio()
 
 
-test.add_ticker(portfolio)
-test.check_portfolio()
 
-test.remove_ticker(["MSFT", "CGC", "AMZN"])
+test.remove_ticker("MSFT", 222222)
 test.check_portfolio()
--- stonk.py
+++ stonk.py
@@ -5,46 +5,34 @@ class Portfolio:
     
     def __init__(self):
         
-        self.stocks = []
+        self.stocks = {}
 
-    def add_ticker(self, ticker) -> None:
+    def add_ticker(self, ticker, shares=1) -> None:
         """Adds a ticker or tickers to the list of stocks. Can either be a single ticker
         or a list of tickers.
         """ 
-        # Add ability to add X number of shares as well
+        # Add ability to add X number of tickers at a time as well
+        # Add error checking to see if the ticker symbol is valid
         if type(ticker) == str:
             if ticker not in self.stocks:
-                self.stocks.append(ticker)
+                self.stocks[ticker] = []
+                self.stocks[ticker].append(shares)
             else:
-                print(ticker + " has already been added to the portfolio")
-        if type(ticker) == list:
-            for t in ticker:
-                if t not in self.stocks:
-                    self.stocks.append(t)
-                else:
-                    print(t + " has already been added to the portfolio")
+                self.stocks[ticker][0] = self.stocks[ticker][0] + shares
 
-    def remove_ticker(self, ticker) -> None:
+    def remove_ticker(self, ticker, shares=1) -> None:
         """Removes a ticker or tickers from the list from the list. Can be either a
         single ticker or a list.
         """
-        # Add ability to remove X number of shares as well
+        # Add ability to add X number of tickers at a time as well
         if type(ticker) == str:
             if ticker in self.stocks:
-                self.stocks.remove(ticker)
-            else:
-                print(ticker + " is not in the portfolio")
-        if type(ticker) == list:
-            for t in ticker:
-                if t in self.stocks:
-                    self.stocks.remove(t)
+                if self.stocks[ticker][0] > shares:
+                    self.stocks[ticker][0] = self.stocks[ticker][0] - shares
                 else:
-                    print(t + " is not in the portfolio")
-
-    def check_portfolio(self) -> list:
-        """Returns the current holdings and their most recent price."""
-        
-        print(self.stocks)
+                    self.stocks.pop(ticker)
+            else:
+                print("Unable to remove, " + ticker + " not found.")
 
     def convert_cad(self, ttp) -> dict:
         """Returns a new dictionary with all prices in CAD."""
@@ -53,13 +41,13 @@ class Portfolio:
 
         for stock in ttp:
             if stock[-2:] != "TO":
-                ttp[stock] = ttp[stock] * CAD
+                ttp[stock][1] = (ttp[stock][1] * CAD).round(2)
 
         return ttp
         
-    def current_prices(self) -> dict:
+    def current_prices(self) -> None:
         """Returns the current prices attached to the individual ticker in a dictionary."""
-
+        """
         ticker_to_price = {}
 
         for stock in self.stocks:
@@ -70,10 +58,28 @@ class Portfolio:
         ticker_to_price = self.convert_cad(ticker_to_price)
 
         return ticker_to_price
+        """
+        for stock in self.stocks:
+            initialize = yf.Ticker(stock)
+            price = initialize.history(period="max")["Close"][-1]
+            if len(self.stocks[stock]) == 1:
+                self.stocks[stock].append(price)
+            elif len(self.stocks[stock]) == 0:
+                None
+            else:
+                self.stocks[stock][1] == price
+
+        self.stocks = self.convert_cad(self.stocks)
+
+    def check_portfolio(self) -> list:
+        """Returns the current holdings and their most recent price."""
+        
+        self.current_prices()
+        print(self.stocks)
 
     def portfolio_value(self) -> float:
         """Returns the total value of all current holdings"""
-
+        
     
 
 
