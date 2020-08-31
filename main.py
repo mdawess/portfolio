6e933ecde324a6c3259a56f96b916c628cd63ebc
mdawess <64979871+mdawess@users.noreply.github.com>
2020-08-30 21:13:17 -0400

    commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from stonk import Portfolio
 from analysis import Analyze
-import tkinter as tk
-import dearpygui as gui
+from tkinter import *
+
 
 # ref https://pypi.org/project/dearpygui/
 
@@ -13,18 +13,19 @@ PORTFOLIO = ["CGC", "TD.TO", "WCN", "BMO.TO", "BNS.TO", "NFI.TO", "VCN.TO",
 current_shares = [10, 20, 35, 5, 12, 1, 11, 6, 1, 1, 2]
 
 myPortfolio = Portfolio()
-myPortfolio.Owner = "Michael Dawes"
-
-# for i in range(len(PORTFOLIO) - 1):
-#     myPortfolio.add_ticker(PORTFOLIO[i], current_shares[i])
-# myPortfolio.add_ticker("SPY")
-# myPortfolio.add_ticker("MSFT", 3)
-# myPortfolio.add_ticker("AAPL", 2)
-
-# myPortfolio.add_ticker("TSLA", 2)
-# myPortfolio.add_ticker("XYZ", 2)
+myPortfolio.Owner = "Michael"
+myPortfolio.add_ticker("AAPL")
 
 test = Analyze(myPortfolio.check_portfolio(), myPortfolio.portfolio_value())
-test.display_charts()
+# test.display_charts()
 
+window = Tk()
 
+window.title(myPortfolio.Owner + "'s Portfolio")
+window.geometry("800x800")
+
+btn1 = Button(window, text="Click to add a stock to the portfolio", bg="blue", fg="red", 
+              COMMAND="myPortfolio.add_ticker(str(input('Please enter a ticker you would like to add to the portfolio')))")
+btn1.place(x=80, y=100)
+
+window.mainloop()
